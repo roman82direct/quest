82e129f5b1b61a82dc2aba092e90f01b5cb80ed2
Roman <roman82direct@yandex.ru>
2020-08-21 18:48:39 +0300

    quest

--- gameCode.js
+++ gameCode.js
@@ -36,32 +36,10 @@ switch (event) {
             case 1: // Второе действие, если во 2 окне ввели 1 то переходим на 4 окно
             answers.push(works.b1);
             getResult();
-                // do {
-                //     ok = false;
-                //     event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
-                //     if (event == -1) {
-                //         break;
-                //     }
-                //     else {
-                //         ok = isAnswer(works.d0, event);
-                //     }
-                // } while (!ok);
-
                 break;
             case 2: // Второе действие   Если ввели 2 то также переходим на 4 окно
             answers.push(works.b2);
             getResult();
-                // do {
-                //     ok = false;
-                //     event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
-                //     if (event == -1) {
-                //         break;
-                //     }
-                //     else {
-                //         ok = isAnswer(works.d0, event);
-                //     }
-                // } while (!ok);
-
                 break;
             case -1: // Второе действие
                 break;
@@ -88,32 +66,10 @@ switch (event) {
             case 1: // Второе действие
             answers.push(works.c1);
             getResult();
-                // do {
-                //     ok = false;
-                //     event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
-                //     if (event == -1) {
-                //         break;
-                //     }
-                //     else {
-                //         ok = isAnswer(works.d0, event);
-                //     }
-                // } while (!ok);
-
                 break;
             case 2: // Второе действие
             answers.push(works.c2);
             getResult();
-                // do {
-                //     ok = false;
-                //     event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
-                //     if (event == -1) {
-                //         break;
-                //     }
-                //     else {
-                //         ok = isAnswer(works.d0, event);
-                //     }
-                // } while (!ok);
-
                 break;
             case -1: // Второе действие
                 break;
@@ -126,7 +82,6 @@ switch (event) {
     default:
         alert('Ошибка');
 }
-// alert('Спасибо за игру');
 
 //------------------------------------------
 
